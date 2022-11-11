4b729a7ec3ce63058b69fa1f3ecf31fbd9c96180
MrDonutLlama <isityouarchagent@gmail.com>
2022-11-11 11:17:15 -0700

    Added todo list for configBuilder.sh, removed more unneccesary items inside of config files

---
+++ scripts/configBuilder.sh
@@ -0,0 +1,3 @@
+#Should be run automatically after bootstrapper.sh finishes its installations
+#Builds (or edits) the user config files for i3, i3blocks, and other associated applications
+#make read top down from each file, and programatically generate only what is needed in the files to minimize runtime and filesize
